19d24e912b12686aa5239f6d99d83d5243679dc5
bruno_h_m <44076984+kur0Black@users.noreply.github.com>
2023-10-17 23:01:06 -0300

    Update admin-script.js // Correções DB

--- admin-script.js
+++ admin-script.js
@@ -1,14 +1,5 @@
-// Função para exibir a seção desejada e ocultar as demais
-function showSection(sectionId) {
-    const sections = document.querySelectorAll('section');
-    for (let section of sections) {
-        if (section.id === sectionId) {
-            section.style.display = 'block';
-        } else {
-            section.style.display = 'none';
-        }
-    }
-}
+const oracledb = import('oracledb');
+//const oracledb = __importDefault(require("oracledb"));
 
 // Oculta todas as seções no carregamento inicial e exibe apenas a primeira
 document.addEventListener('DOMContentLoaded', function() {
@@ -22,6 +13,27 @@ document.getElementById('form-aeronaves').addEventListener('submit', function(ev
     const modelo = document.getElementById('modelo').value;
     const identificacao = document.getElementById('identificacao').value;
 
+    try {
+        const connection = oracledb.getConnection({
+            user: 'System',
+            password: 'senha',
+            connectString: 'localhost:1521/xe',
+        });
+
+        const sql = 'INSERT INTO Aeronaves (modelo, identificacao, fabricante, ano) VALUES (:modelo, :identificacao, :fabricante, :ano)';
+        const binds = { modelo, identificacao, fabricante, ano };
+
+        const options = {
+            autoCommit: true,
+        };
+
+        const result = connection.execute(sql, binds, options);
+        console.log('Inserção bem-sucedida');
+        connection.close();
+    } catch (error) {
+        console.error('Erro ao inserir no Oracle:', error);
+    }
+
     const listItem = document.createElement('li');
     listItem.textContent = `${modelo} - ${identificacao}`;
 
@@ -30,33 +42,21 @@ document.getElementById('form-aeronaves').addEventListener('submit', function(ev
     // Limpa o formulário
     event.target.reset();
 });
-document.getElementById('form-aeronaves').addEventListener('submit', function (event) {
-    event.preventDefault();
-  
-    const modelo = document.getElementById('modelo').value;
-    const identificacao = document.getElementById('identificacao').value;
-    const fabricante = document.getElementById('fabricante').value;
-    const anoFabricacao = document.getElementById('ano-fabricacao').value;
-  
-    fetch('/inserir-aeronave', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ modelo, identificacao, fabricante, anoFabricacao }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        alert(data.message);
-      })
-      .catch((error) => {
-        console.error(error);
-        alert('Erro ao inserir aeronave no banco de dados.');
-      });
-  
-    // Limpa o formulário
-    event.target.reset();
-  });
+
+
+// Função para exibir a seção desejada e ocultar as demais
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('section');
+    for (let section of sections) {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    }
+}
+
+
 
 // Funcionalidade para o formulário de aeroportos
 document.getElementById('form-aeroportos').addEventListener('submit', function(event) {
@@ -64,6 +64,28 @@ document.getElementById('form-aeroportos').addEventListener('submit', function(e
 
     const aeroporto = document.getElementById('aeroporto').value;
     const cidade = document.getElementById('cidade').value;
+
+    try {
+        const connection = oracledb.getConnection({
+            user: 'System',
+            password: 'senha',
+            connectString: 'localhost:1521/xe',
+        });
+
+        const sql = 'INSERT INTO Aeroportos (aeroporto, cidade) VALUES (:aeroporto, :cidade)';
+        const binds = { aeroporto, cidade };
+
+        const options = {
+            autoCommit: true,
+        };
+
+        const result = connection.execute(sql, binds, options);
+
+        console.log('Inserção bem-sucedida');
+        connection.close();
+    } catch (error) {
+        console.error('Erro ao inserir no Oracle:', error);
+    }
 
     const listItem = document.createElement('li');
     listItem.textContent = `${aeroporto} - ${cidade}`;
@@ -81,6 +103,28 @@ document.getElementById('form-trechos').addEventListener('submit', function(even
     const origem = document.getElementById('origem').value;
     const destino = document.getElementById('destino').value;
 
+    try {
+        const connection = oracledb.getConnection({
+            user: 'System',
+            password: 'senha',
+            connectString: 'localhost:1521/xe',
+        });
+
+        const sql = 'INSERT INTO Trechos (origem, destino) VALUES (:origem, :destino)';
+        const binds = { origem, destino };
+
+        const options = {
+            autoCommit: true,
+        };
+
+        const result = connection.execute(sql, binds, options);
+
+        console.log('Inserção bem-sucedida');
+        connection.close();
+    } catch (error) {
+        console.error('Erro ao inserir no Oracle:', error);
+    }
+    
     const listItem = document.createElement('li');
     listItem.textContent = `${origem} -> ${destino}`;
 
@@ -102,6 +146,30 @@ document.getElementById('form-voos').addEventListener('submit', function(event) 
     const aeroportoChegada = document.getElementById('aeroporto-chegada').value;
     const valorPassagem = document.getElementById('valor-passagem').value;
 
+
+    try {
+        const connection = oracledb.getConnection({
+            user: 'System',
+            password: 'senha',
+            connectString: 'localhost:1521/xe',
+        });
+
+        const sql = 'INSERT INTO Voos (numeroVoo, dataVoo, horarioPartida, horarioChegada, aeroportoSaida, aeroportoChegada, valorPassagem) VALUES (:numeroVoo, :dataVoo, :horarioPartida, :horarioChegada, :aeroportoSaida, :aeroportoChegada, :valorPassagem)';
+        const binds = { numeroVoo, dataVoo, horarioPartida, horarioChegada, aeroportoSaida, aeroportoChegada, valorPassagem };
+
+        const options = {
+            autoCommit: true,
+        };
+
+        const result = connection.execute(sql, binds, options);
+
+        console.log('Inserção bem-sucedida');
+        connection.close();
+    } catch (error) {
+        console.error('Erro ao inserir no Oracle:', error);
+    }
+
+
     const listItem = document.createElement('li');
     listItem.textContent = `Voo ${numeroVoo} - ${dataVoo} - ${horarioPartida}-${horarioChegada} - ${aeroportoSaida}-${aeroportoChegada} - R$ ${valorPassagem}`;
 
@@ -110,7 +178,4 @@ document.getElementById('form-voos').addEventListener('submit', function(event) 
     // Limpa o formulário
     event.target.reset();
 });
-
-
-
 
